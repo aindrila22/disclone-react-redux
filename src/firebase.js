fa96c2b7f4288091e99e1724e3dbc6b52d55cba9
Aindrila Bhattacharjee <47319230+aindrila22@users.noreply.github.com>
2020-10-15 10:58:50 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAgotvv4yqffGphuLwuMc1WDMOAaC3I5dU",
-    authDomain: "discordclone-3eee6.firebaseapp.com",
-    databaseURL: "https://discordclone-3eee6.firebaseio.com",
-    projectId: "discordclone-3eee6",
-    storageBucket: "discordclone-3eee6.appspot.com",
-    messagingSenderId: "901068499949",
-    appId: "1:901068499949:web:2d5c4f4fd8ec5be96595e4"
-  };
+}
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   const db = firebaseApp.firestore();
